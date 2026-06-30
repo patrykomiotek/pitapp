5bdd1a942bfd731a945f3094309d9fbdf1accc77
Patryk Omiotek <patryk.omiotek@gmail.com>
2026-06-30 09:47:44 +0200

    feat: with ui barrels

--- src/components/Generator.tsx
+++ src/components/Generator.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
-import { Button } from "../ui/Button";
-import { Text } from "../ui/Text";
+import { Button, Text } from "../ui";
 
 const Generator = () => {
   const [value, setValue] = useState("");
---
+++ src/ui/index.ts
@@ -0,0 +1,7 @@
+import { Button } from "./Button";
+import { Text } from "./Text";
+
+export { Button, Text };
+
+// export { Button } from "./Button";
+// export { Text } from "./Text";
