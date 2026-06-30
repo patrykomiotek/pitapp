c4cf13175a1a33a43f606d275f34da054e738f5e
Patryk Omiotek <patryk.omiotek@gmail.com>
2026-06-30 11:36:52 +0200

    feat: counter component

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,15 +1,18 @@
 import { useEffect, useState } from "react";
 import { Text } from "../ui";
 
+// componentDidMount, componentDidUpdate, componentWillUnmount
 export const Counter = () => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
+    // montowania albo update
     const id = setInterval(() => {
       setCounter((value) => value + 1);
     }, 1000);
 
     return () => {
+      // odmontowywanie
       // destruktor
       clearInterval(id);
     };
