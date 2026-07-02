95f6e5ee639aa4c5b97a817a855b8b2030a10ccd
Patryk Omiotek <patryk.omiotek@gmail.com>
2026-07-02 13:30:47 +0200

    fix: revert marker

--- src/shared/components/AppMap.tsx
+++ src/shared/components/AppMap.tsx
@@ -9,11 +9,11 @@ export const AppMap = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {/* <Marker position={[51.505, -0.09]}>
+        <Marker position={[51.505, -0.09]}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
-        </Marker> */}
+        </Marker>
       </MapContainer>
     </div>
   );
