0eb2ec06ea11189179192ffee4f59406eb8ae5c3
Patryk Omiotek <patryk.omiotek@gmail.com>
2026-07-02 14:08:00 +0200

    fix: deault map zoom

--- src/shared/components/Map/AppMap.tsx
+++ src/shared/components/Map/AppMap.tsx
@@ -20,7 +20,7 @@ export const AppMap = () => {
 
   return (
     <div>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer center={[51.505, -0.09]} zoom={6} scrollWheelZoom={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
